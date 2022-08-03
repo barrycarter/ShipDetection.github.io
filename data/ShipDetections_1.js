eb647fbff70aa4f40437b6f84e806402358c33d0
Drew Branson <37157743+drewbranson@users.noreply.github.com>
2022-08-02 21:37:33 -0700

    maybe?

--- data/ShipDetections_1.js
+++ data/ShipDetections_1.js
@@ -1,6 +1,1 @@
-// $.ajaxPrefilter( "json script", function( options ) {
-//     options.crossDomain = true;
-// });
-
-
-var json_ShipDetections_1 = JSON.parse($.ajax({'url': 'data/geodata.json', 'async': false}).responseText); 
+var json_ShipDetections_1 = JSON.parse($.ajax({'url': 'data/geodata.geojson', 'async': false}).responseText); 
