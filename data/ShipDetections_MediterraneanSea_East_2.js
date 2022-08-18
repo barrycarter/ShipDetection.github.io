e7edc551815a83f6af788960e06c507f2db20d8e
Drew Branson <37157743+drewbranson@users.noreply.github.com>
2022-08-18 01:00:15 -0700

    update

---
+++ data/ShipDetections_MediterraneanSea_East_2.js
@@ -0,0 +1,27 @@
+var json_ShipDetections_MediterraneanSea_East_2 = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "field_1": "110",
+        "ShipDetections": "target_110",
+        "Detected_width": "180.0",
+        "Detected_length": "140.0",
+        "Date": "20220201",
+        "Year": "2022",
+        "Month": "02",
+        "Day": "01",
+        "Time": "034912",
+        "DaysOld": "100.0"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          36.558682694396005,
+          45.13714781013336
+        ]
+      }
+    }
+    ]
+}
