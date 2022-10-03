bbce2e124b180cc32ce85b4884be93c9a0889ce0
Drew Branson <37157743+drewbranson@users.noreply.github.com>
2022-10-03 03:08:15 -0700

    update

--- data/ShipDetections_BlackSea_buffer200_1.js
+++
@@ -1,4 +0,0 @@
-var json_ShipDetections_BlackSea_buffer200_1 = {
-  "type": "FeatureCollection",
-  "features": []
-}
---
+++ data/ShipDetections_Mediterranean_East_1.js
@@ -0,0 +1,4 @@
+var json_ShipDetections_Mediterranean_East_1 = {
+  "type": "FeatureCollection",
+  "features": []
+}
